fd3d7364d9518f3aed93c80e3e3864ff61e6689d
Sengthai <sengthai37@gmail.com>
2023-12-12 02:09:57 +0000

    Merge to sengthai branch

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from qiskit import *
 from src import RunQPlayer
+from qplayer_wra import *
 
-qc = QuantumCircuit(2, 2)
+qc = QuantumCircuit(2)
 qc.h(0)
-qc.cy(0, 1)
-qc.rx(2, 0)
-qc.measure(0,0)
-qc.measure(1,1)
+qc.h(1)
+qc.y(0)
+qc.cx(0, 1)
+qc.measure_all()
 
 qasm = qc.qasm()
 
-result = RunQPlayer(qasm, run=1080)
+result = executeQASM(qasm, 1080)
+# result = RunQPlayer(qasm, 1080)
 print(result)
